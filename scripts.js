e253ed2d6435a59100de3c6cb0fdce702d179d98
Cristolfe <82662425+Icristolfe@users.noreply.github.com>
2022-12-09 11:49:21 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ const convertValue = async () => {
     const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( response => response.json())
     const dolar = data.USDBRL.high
     const euro = data.EURBRL.high
-    const bitcoin = 90.30553
+    const bitcoin = 9030553
 
     realValue.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
